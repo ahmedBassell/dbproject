895cece782a6475e169b50397d83c155f88bb275
Ahmed Bassell <ahmedbassell@gmail.com>
2015-12-24 21:36:29 +0200

    signup Done

--- project/controllers/student.php
+++ project/controllers/student.php
@@ -22,9 +22,10 @@ class student
 		// submit data
 		include('models/student_model.php');
 		$student_model = new student_model();
-		if($student_model->signup($student))
+		if($student_id = $student_model->signup($student))
 		{
-			include('views/home.php');
+			$_SESSION['student_id'] = $student_id;
+			header('Location: '.$this->base_url('student/home'));
 		}
 		else
 		{
@@ -32,10 +33,28 @@ class student
 		}
 		
 	}
+
+
 	public function base_url($segment)
 	{
 		$base_url = $this->bu;
 		$url = $base_url.$segment;
 		return $url;
+	}
+
+
+
+	// private
+	public function home()
+	{
+		$student_id = $_SESSION['student_id'] ;
+		$student = (object) array_shift( $this->get_student($student_id) );
+		include('views/home.php');
+	}
+	private function get_student($student_id)
+	{
+		include('models/student_model.php');
+		$student_model = new student_model();
+		return $student_model->get_student($student_id);
 	}
 }
--- project/models/student_model.php
+++ project/models/student_model.php
@@ -14,8 +14,16 @@ class student_model extends DatabaseConnect
 		$sql .=	" VALUES ('{$student->name}', '{$student->address}', '{$student->email}', '{$student->password}', ";
 		$sql .=	" '{$student->department}', '{$student->year}', '{$student->grad}', '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."') ";
 		$result = $this->db->query($sql);
-		echo $sql;
-		var_dump($result);
+		$last_id = $this->db->insert_id ;
+		$result = $result? $last_id:$result;
+		return $result;
+	}
+
+	public function get_student($student_id)
+	{
+		$sql  = "SELECT * FROM student WHERE id = {$student_id}";
+		$result = $this->db->query($sql);
+		$result = $this->result($result);
 		return $result;
 	}
 }
