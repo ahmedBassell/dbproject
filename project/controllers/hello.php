f9e934f27fd1b02dbed2b3c670c51aa496589adb
Ahmed Bassell <ahmedbassell@gmail.com>
2015-12-25 22:30:36 +0200

    fix require

--- project/controllers/hello.php
+++ project/controllers/hello.php
@@ -1,17 +1,24 @@
 <?php
-require('config/controller.php');
+require_once('config/controller.php');
 class hello extends controller
 {
 
 	public function index()
 	{	
-		include('views/hello.php');
+		if(!isset($_SESSION['student_id']))
+		{
+			require_once('views/hello.php');	
+		}
+		else
+		{
+			header('Location: '.$this->base_url('student/home'));
+		}
 	}
 	// example function for calling model and calling view
 	public function employees()
 	{
 		
-		require('models/employee_model.php');
+		require_once('models/employee_model.php');
 		$emp = new employee_model();
 		$employees = $emp->get_employees();
 		require('views/employees.php');
